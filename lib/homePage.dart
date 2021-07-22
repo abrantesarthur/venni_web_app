624e0a49b8454b3fe8230448edfc8658568ace90
arthur_abrantes <arthur@venni.app>
2021-07-22 20:19:17 -0300

    add images

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:venni_website/styles.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.pink,
+        color: AppColors.primaryPink,
         alignment: Alignment.topCenter,
         child: SingleChildScrollView(
           child: Column(
