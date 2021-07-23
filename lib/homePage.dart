8cefb8f2dccbc0f3eee39af691477a978f0a6c49
arthur_abrantes <arthur@venni.app>
2021-07-22 21:12:21 -0300

    add fonts

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_svg/svg.dart';
 import 'package:venni/styles.dart';
+import 'package:venni/widgets/appButton.dart';
 import 'package:venni/widgets/overallPadding.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       body: Container(
@@ -17,14 +19,70 @@ class HomePage extends StatelessWidget {
             child: Column(
               children: [
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     SvgPicture.asset(
                       "images/logo-horizontal-white.svg",
                       width: screenWidth / 5,
                     ),
+                    AppButton(
+                      textData: "Trabalhe Conosco",
+                      hasShadow: false,
+                      border: Border.all(color: Colors.white, width: 2),
+                      onTapCallBack: () {},
+                    )
                   ],
                 ),
+                SizedBox(height: screenHeight / 5),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          RichText(
+                            textAlign: TextAlign.start,
+                            text: TextSpan(
+                              text: "Baixe o app e faça viajens de moto com",
+                              style: TextStyle(
+                                fontSize: 64,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w100,
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: " segurança",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w900,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: " e por um ",
+                                ),
+                                TextSpan(
+                                  text: " preço justo",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w900,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: screenHeight / 10),
+                          Text(
+                            "hello",
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text("Hello"),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
